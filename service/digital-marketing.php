3d60ca745c6d6b8eccfd0bbf061c6e30d7e4a0d3
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-12 17:47:20 +0530

    include/header and menu inside footer section add link

--- service/digital-marketing.php
+++ service/digital-marketing.php
@@ -23,7 +23,7 @@
     <!--end fixed header-->
 	<!-- header -->
 	<section class="home-5-banner bg-img main-banner pt-5 mt-5 pb-0" id="banner">
-		<div class="container-fluid px-md-5 px-3 pt-5">
+		<div class="container-fluid px-md-5 pl-4 pr-5 pt-5">
 			<div class="row align-items-center">
 				<div class="col-lg-6 col-md-7">
 					<div class="banner-content">
@@ -58,7 +58,7 @@
 	</section><!-- banner -->
 
 		<section class="feature">
-		<div class="container-fluid px-md-5 px-3">
+		<div class="container-fluid px-md-5 pl-4 pr-5">
 				<div class="s-title mb-4 mt-0">
 					<h2><span class="logo-color">W</span>HY CHOOSE US?</h2>
 			</div>
@@ -150,7 +150,7 @@
 	</section><!-- feature -->
 
 	<section class="about s-padding pt-5">
-		<div class="container-fluid px-md-5 px-3 pt-5">
+		<div class="container-fluid px-md-5 pl-4 pr-5 pt-5">
 			<div class="row align-items-center">
 				<div class="col-lg-5 mb-3">
 					<div class="about-content">
@@ -169,7 +169,7 @@
 	</section><!-- about -->	
 
 	<section class="develp-Area wow slideInUp  py-md-0" data-wow-delay=".1s" style="visibility: visible; animation-delay: 0.1s; animation-name: slideInUp;">
-    	  <div class="container-fluid px-md-5 px-3">
+    	  <div class="container-fluid px-md-5 pl-4 pr-5">
     	  	   <h2 class="w-100 d-block text-center pb-md-4 pb-2">Digital Marketing</h2>
     	  	   <div class="row pt-3">
     	  	   	    <div class="col-md-4 mb-4">
@@ -247,7 +247,7 @@
     </section>
 
 	<section class="quick-overview s-padding pb-4">
-		<div class="container-fluid pl-md-5">
+		<div class="container-fluid px-md-5 pl-4 pr-5">
 			<div class="s-title mt-0 mb-4">
 				<h2><span class="logo-color">Q</span>uick Overview</h2>
 			</div>
