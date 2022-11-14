1d637659a7efd6b52f6d597e246c714cbb94a1bc
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 13:54:28 +0530

    include/header and menu inside footer section add link

--- service/mlm-software-development.php
+++ service/mlm-software-development.php
@@ -375,7 +375,7 @@ manages all the affairs of the business.
 		<div class="s-title">
 			<h2>Frequently Asked Question</h2>
 		</div>
-		<div class="container-fluid">
+		<div class="container">
 
 			<div class="row align-items-center">
 
