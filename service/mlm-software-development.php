f784509e14df3256c4f2a8747836685e4a610714
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 13:31:20 +0530

    include/header and menu inside footer section add link

--- service/mlm-software-development.php
+++ service/mlm-software-development.php
@@ -23,7 +23,6 @@
     <!--end fixed header-->
 	<!-- header -->
 	<section class="home-5-banner bg-img main-banner pt-5 mt-5 pb-3" id="banner">
-
 		<div class="container-fluid px-md-5 px-3 pt-5">
 
 			<div class="row align-items-center">
