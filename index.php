6b04b15fff6974d2a494b78435f0716ce5925667
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 16:15:04 +0530

    include/header and menu inside footer section add link

--- index.php
+++ index.php
@@ -195,7 +195,7 @@
 
                                           </div>
 
-                                          <div class="col-md-6 prod-pt">
+                                          <div class="col-md-6 prod-pt" style="animation: fadeInRight 1s ease-in-out;">
 
                                                 <h2 class="w-100">Odoo ERP</h2>
 
