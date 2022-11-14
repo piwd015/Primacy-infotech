0dc574a4bd9e05aab10578bcfa38b6c273a77f2c
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 12:56:43 +0530

    include/header and menu inside footer section add link

--- service/ui-ux-desing.php
+++ service/ui-ux-desing.php
@@ -46,7 +46,7 @@
 		</div>
 	</section><!-- banner -->
 
-	<section class="feature pt-md-0 pt-5 mt-md-0 mt-5">
+	<section class="feature pt-md-0 pt-5 ">
 		<div class="container-fluid px-md-5">
 			<div class="row justify-content-center">
 				<div class="col-lg-4">
