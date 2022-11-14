67b92d71b143bcd2e944458194b9ae6aff929df9
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 11:17:27 +0530

    include/header and menu inside footer section add link

--- service/crm-erp-portal-development.php
+++ service/crm-erp-portal-development.php
@@ -59,7 +59,7 @@
 	</section>
 	<!-- banner -->	
 
-	<section class="h4-about s-padding pb-3">
+	<section class="h4-about s-padding pb-3 pt-md-5 pt-3">
 		<div class="container-fluid px-md-5 px-4">
 
 			<div class="row align-items-center">
