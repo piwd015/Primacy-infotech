ef5673747b88f0e733bc167117327aeeb13ff939
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-14 12:33:32 +0530

    include/header and menu inside footer section add link

--- service/ui-ux-desing.php
+++ service/ui-ux-desing.php
@@ -38,7 +38,7 @@
 					</div>
 				</div>
 
-				<div class="col-lg-5 col-md-3 offset-lg-1 offset-md-2 banner-img-wrapper">
+				<div class="col-lg-5 col-md-3 offset-lg-1 offset-md-2 Ul-uxImg">
 					<img src="../assets/images/banner-mobile1.png" class="banner-img-1 wow slideInUpAlt" data-wow-delay=".1s" alt="#primacy">
 					<img src="../assets/images/banner-mobile2.png" class="banner-img-2 wow slideInRightAlt" alt="#primacy">
 				</div>
