9d5b1d48d684538cfb37caa81eff9c0cc43d3e58
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-15 13:27:24 +0530

    include/header and menu inside footer section add link

--- about/about.php
+++ about/about.php
@@ -87,7 +87,6 @@
     <section class="develp-Area wow slideInUp py-md-5" data-wow-delay=".1s" style="visibility: visible; animation-delay: 0.1s; animation-name: slideInUp;">
             <div class="container-fluid px-md-5">
                     <h4 class="w-100 d-block text-center">Why We Exit</h4>
-                    <h2 class="w-100 d-block text-center pb-md-4 pb-2">Why We Exit</h2>
                     <span class="d-block text-center">We, unlike robots, do not work only with our brains. We are humans with an uncanny love for
                         technology and immense love for solutions.</span>
                    <ul class="crt-vlu w-100 d-block text-center pt-3">
