2108cddd014ff571d292e259f5db09fdafee0598
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-15 12:41:32 +0530

    include/header and menu inside footer section add link

--- index.php
+++ index.php
@@ -1049,7 +1049,7 @@
 
         <div class="mb-page">
 
-            <section class="inovton-bg-02 w-100 d-block pt-5" data-aos="fade-down">
+            <section class="inovton-bg-02 w-100 d-block pt-5 pb-3" data-aos="fade-down">
 
                         <div class="container-fluid pt-5">
 
