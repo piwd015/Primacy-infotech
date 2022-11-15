b6c2f5209339baa4b4f56a39f5a854779d0fc90d
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-15 19:07:59 +0530

    include/header and menu inside footer section add link

--- about/about.php
+++ about/about.php
@@ -177,7 +177,7 @@
                          </div>
                          <div class="col-md-3">
                            <div class="team w-100 d-block text-center">
-                               <div class="team-circle" style="background:#f78b33;">
+                               <div class="team-circle" style="background:#a327a4;">
                                    <a href="https://www.facebook.com/primacyit/" class="fb" target="_blank"><i class="fab fa-facebook-f"></i></a>
                                    <a href="https://twitter.com/primacyinfotech" class="tw" target="_blank"><i class="fab fa-twitter"></i></a>
                                    <a href="https://www.instagram.com/primacyinfotech.in/?hl=en" class="ins" target="_blank"><i class="fab fa-instagram"></i></a>
@@ -190,7 +190,7 @@
                        </div>
                          <div class="col-md-3">
                            <div class="team w-100 d-block text-center">
-                               <div class="team-circle" style="background:#f78b33;">
+                               <div class="team-circle" style="background:#17928d;">
                                    <a href="https://www.facebook.com/primacyit/" class="fb" target="_blank"><i class="fab fa-facebook-f"></i></a>
                                    <a href="https://twitter.com/primacyinfotech" class="tw" target="_blank"><i class="fab fa-twitter"></i></a>
                                    <a href="https://www.instagram.com/primacyinfotech.in/?hl=en" class="ins" target="_blank"><i class="fab fa-instagram"></i></a>
