7366ac85a821784530e2b5897004b3fa2d517ae3
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-16 20:03:23 +0530

    include/header and menu inside footer section add link

--- include/footer.php
+++ include/footer.php
@@ -146,18 +146,6 @@
 
 </script> 
 
-<script>
-    $(document).ready(function(){
-        $('ul.tabs li').click(function(){
-            var tab_id= $(this).attr('data-tab');
-            $('ul.tabs li').removeClass('current');
-            $('.tab-content').removeClass('current');
-            $(this).addClass('current');
-            $("#"+tab_id).addClass('current');
-
-        })
-    })
-</script>
 
 <script>
 
