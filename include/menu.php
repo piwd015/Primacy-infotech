cafdc9b93377d3c7c2ff9b84610f0f093733e1b8
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-17 19:16:22 +0530

    include/header and menu inside footer section add link

--- include/menu.php
+++ include/menu.php
@@ -71,7 +71,7 @@
                             <a href="../oddo/oddo-implementation"><i class="icofont-print icofont-2x"></i><br />
                                 <div class="txt">Odoo Implementation</div></a>
                         </li>
-                        <li><a href="../oddo/odoo-consulting-services"><i class="icofont-architecture-alt icofont-2x"></i>
+                        <li><a href="#"><i class="icofont-architecture-alt icofont-2x"></i>
                                 <div class="txt">Odoo Consulting Services</div>
                             </a>
                         </li>
