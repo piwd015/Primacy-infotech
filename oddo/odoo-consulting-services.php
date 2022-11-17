482e7a02867d9227e832f8b71772c93c1e0a2f4f
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-17 11:53:54 +0530

    include/header and menu inside footer section add link

--- oddo/odoo-consulting-services.php
+++ oddo/odoo-consulting-services.php
@@ -38,7 +38,7 @@
                   <h1 class="w-100 d-block text-center pt-5 pb-2">Odoo Consulting Services</h1>
                    <h4 class="w-100 d-block text-center pb-md-5 pb-3">An Experienced Odoo Consultant at your service!</h4>
                 <div class="row d-flex flex-wrap justify-content-center">
-                      <div class="col-md-4">
+                      <div class="col-md-3">
                            <a href="https://api.whatsapp.com/send?phone=919088015865" class="wp-app" target="_blank"><i class="fab fa-whatsapp mr-2"></i>Talk to our experts</a>
                       </div>
                 </div>
