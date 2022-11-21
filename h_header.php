b69ca78891a2a46a48735aa2206487a150f7fc87
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-21 14:37:18 +0530

    include/header and menu inside footer section add link

--- h_header.php
+++ h_header.php
@@ -44,7 +44,7 @@
 <div class="top" data-aos="fade-down">
     <div class="container-fluid px-0">
         <div class="row">
-                <div class="col-md-2 col-12 pic0"><a href="../"><img src="assets/images/logo-p.png" alt="primacy logo"></a></div>
+                <div class="col-md-2 col-12 pic0"><a href="../"><img src="assets/images/logo-p.png" class="ml-md-0 ml-3" alt="primacy logo"></a></div>
                 <div class="col-md-7 col-12">
                     <div class="c-mob">
                         <div class="col-md-4">
