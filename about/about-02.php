9aebb2f4a9ae2d002af18e5e9d7520ae875eeb10
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-25 12:38:29 +0530

    include/header and menu inside footer section add link

--- about/about-02.php
+++ about/about-02.php
@@ -36,7 +36,9 @@
           <section class="aboutImg-01">
                  <div class="container-fluid">
                        <h1 class="w-100 d-block text-center">Lorem ipsum<br/>
-                       <span class="w-100 d-block text-center">consectetur</span></h1>
+                       <span class="w-100 d-block text-center"><strong>consectetur</strong></span><br/>
+                           <span><strong>sed do</strong> eiusmod tempor</span>
+                       </h1>
                  </div>
           </section>
     </div>
