4c76da37e4b260e359fc56fdaf56eab3b4e5ef67
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-28 19:28:14 +0530

    include/header and menu inside footer section add link

--- product/school-managmest-system.php
+++ product/school-managmest-system.php
@@ -41,7 +41,7 @@
 
 <!--end fixed header-->
 	<!-- header -->
-	<section class="home-5-banner bg-img main-banner pb-5" id="banner">
+	<section class="home-5-banner bg-img main-banner pb-4" id="banner">
 
 		<div class="container-fluid px-md-5 px-3">
 
