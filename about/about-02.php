105707c865f7e9b8f559340abda6469087e81616
Raju Primacy <piwd015@primacyinfotech.com>
2022-11-30 12:27:15 +0530

    include/header and menu inside footer section add link

--- about/about-02.php
+++ about/about-02.php
@@ -238,36 +238,36 @@
                    </div>
             </section>
             <section class="team-bg w-100 d-block py-md-4 py-3">
-                     <div class="team-bg-wht py-md-4 py-3">
-                           <div class="container">
-                                 <div class="row">
-                                      <div class="col-md-3 col-6 mb-3">
-                                              <div class="content">
-                                                  <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
-                                                      <div class="content-overlay"></div>
-                                                      <img class="content-image" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362">
-                                                      <div class="content-details fadeIn-bottom">
-                                                          <h3 class="content-title">This is a title</h3>
-                                                          <p class="content-text">This is a short description</p>
-                                                      </div>
-                                                  </a>
+                   <div class="container">
+                        <div class="team-bg-wht py-md-4 py-3">
+                             <div class="row">
+                              <div class="col-md-3 col-6 mb-3">
+                                      <div class="content">
+                                          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                              <div class="content-overlay"></div>
+                                              <img class="content-image" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362">
+                                              <div class="content-details fadeIn-bottom">
+                                                  <h3 class="content-title">This is a title</h3>
+                                                  <p class="content-text">This is a short description</p>
                                               </div>
+                                          </a>
                                       </div>
-                                      <div class="col-md-3 col-6 mb-3">
-                                              <div class="content">
-                                                  <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
-                                                      <div class="content-overlay"></div>
-                                                      <img class="content-image" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362">
-                                                      <div class="content-details fadeIn-bottom">
-                                                          <h3 class="content-title">This is a title</h3>
-                                                          <p class="content-text">This is a short description</p>
-                                                      </div>
-                                                  </a>
+                              </div>
+                              <div class="col-md-3 col-6 mb-3">
+                                      <div class="content">
+                                          <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                              <div class="content-overlay"></div>
+                                              <img class="content-image" src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362">
+                                              <div class="content-details fadeIn-bottom">
+                                                  <h3 class="content-title">This is a title</h3>
+                                                  <p class="content-text">This is a short description</p>
                                               </div>
+                                          </a>
                                       </div>
-                                 </div>
-                           </div>
-                     </div>
+                              </div>
+                         </div>
+                        </div>
+                   </div>
             </section>
             <section class="delivered-bg">
                   <div class="container">
