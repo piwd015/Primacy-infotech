e32d0f74de51c728afab9a5edae34471c4b6d8c3
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-02 12:43:18 +0530

    include/header and menu inside footer section add link

--- product/ip-attorney-erp.php
+++ product/ip-attorney-erp.php
@@ -70,7 +70,7 @@
         </section>
 
         <!-- ============================ Make Collaboration Start ================================== -->
-        <section class="mission">
+        <section class="mission pb-0">
                  <div class="container">
                         <div class="col-md-12 pt-3 pb-5">
                                <h2 class="w-100 d-block text-center">Patent Docketing Software</h2>
