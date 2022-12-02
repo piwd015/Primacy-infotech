2b7a9bf4edcad3e398e4a379d56ee205342cb38a
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-02 12:12:03 +0530

    include/header and menu inside footer section add link

--- include/common1.php
+++ include/common1.php
@@ -296,7 +296,7 @@
 
         <div class="row">
 
-            <div class="col-md-4 offset-md-1 ">
+            <div class="col-md-7">
 
                 <div class="iconic-contact-info">
 
@@ -370,7 +370,7 @@
 
             </div>
 
-            <div class="col-md-6 ">
+            <div class="col-md-5 ">
 
                 <br/><br/><br/>
 
