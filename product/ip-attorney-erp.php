f01467769f1985e9e16a352aaec21cba528c2fc9
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-05 11:21:42 +0530

    include/header and menu inside footer section add link

--- product/ip-attorney-erp.php
+++ product/ip-attorney-erp.php
@@ -1033,7 +1033,7 @@
                       <div class="row d-flex flex-wrap justify-content-center">
                             <div class="col-md-11">
                                     <div class="col text-center">
-                                        <div class="sec-heading mx-auto" style="max-width:80%;">
+                                        <div class="sec-heading mx-auto prisn-pln">
                                               <h2>Pricing Plans</h2>
                                               <p>IPGO  is developed for larger user base, from an individual
                                                   IP Attorney to a large IP Law Firm. We have adjusted the
