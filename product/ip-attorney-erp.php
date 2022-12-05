4d518c0a4af94b480a8851e207626343922e7c16
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-05 13:26:50 +0530

    include/header and menu inside footer section add link

--- product/ip-attorney-erp.php
+++ product/ip-attorney-erp.php
@@ -190,7 +190,7 @@
                             <div class="col-lg-6 col-md-6 col-sm-12">
                                 <div class="large-features-2 style-1 shadow-0 border-0" data-aos="fade-left" data-aos-duration="1200">
                                     <div class="large-features-2-thumb">
-                                        <img src="../assets/images/offer_icon10.png" class="img-responsive" alt="" />
+                                        <img src="../assets/images/product/multi-user.png" class="img-responsive" alt="" />
                                     </div>
                                     <div class="large-features-2-detail pt-md-0 pt-5">
                                         <h4>Multi User</h4>
@@ -201,7 +201,7 @@
                             <div class="col-lg-6 col-md-6 col-sm-12">
                                 <div class="large-features-2 style-1 shadow-0 border-0" data-aos="fade-left" data-aos-duration="1200">
                                     <div class="large-features-2-thumb">
-                                        <img src="../assets/images/offer_icon10.png" class="img-responsive" alt="" />
+                                        <img src="../assets/images/product/multi-user.png" class="img-responsive" alt="" />
                                     </div>
                                     <div class="large-features-2-detail pt-md-0 pt-5">
                                         <h4>Multi Branch</h4>
