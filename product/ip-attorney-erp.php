ade429a127252f40479bb530b4ff029bf323f86b
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-06 12:19:18 +0530

    include/header and menu inside footer section add link

--- product/ip-attorney-erp.php
+++ product/ip-attorney-erp.php
@@ -483,40 +483,40 @@
                                                                             <img src="../assets/images/product/trademark-renewal.jpg" alt="features icon" />
                                                                         </div>
                                                                         <div class="veticle-features-content">
-                                                                            <h4><strong style="color:#bd392f;">Trademark Renewa</strong><dd>Trademark renewal history with due date reminder</dd></h4>
+                                                                            <h4><strong style="color:#bd392f;">Trademark Renewal</strong><dd>Trademark renewal history with due date reminder</dd></h4>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                                 <!-- Single Features -->
                                                                 <div class="veticle-features float-left mb-0">
                                                                     <div class="veticle-features-item" style="justify-content:left;">
-                                                                        <div class="veticle-large-features-box">
-                                                                            <img src="../assets/images/features_icon_1.png" alt="features icon" />
+                                                                        <div class="trdmrk-box">
+                                                                            <img src="../assets/images/product/trademark-renewal.jpg" alt="features icon" />
                                                                         </div>
                                                                         <div class="veticle-features-content">
-                                                                            <h4>Reports<dd>Advanced reporting with state-of-the art search features</dd></h4>
+                                                                            <h4><strong style="color:#bd392f;">Reports</strong><dd>Advanced reporting with state-of-the art search features</dd></h4>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                                 <!-- Single Features -->
                                                                 <div class="veticle-features mb-0">
                                                                     <div class="veticle-features-item" style="justify-content:left;">
-                                                                        <div class="veticle-large-features-box">
-                                                                            <img src="../assets/images/features_icon_2.png" alt="features icon" />
+                                                                        <div class="trdmrk-box">
+                                                                            <img src="../assets/images/product/clnt-asso-management.jpg" alt="features icon" />
                                                                         </div>
                                                                         <div class="veticle-features-content">
-                                                                            <h4>Clients & Associate Management<dd>Clients and associates with multiple contacts per client</dd></h4>
+                                                                            <h4><strong style="color:#9bbb5c;">Clients & Associate Management</strong><dd>Clients and associates with multiple contacts per client</dd></h4>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                                 <!-- Single Features -->
                                                                 <div class="veticle-features mb-0">
                                                                     <div class="veticle-features-item" style="justify-content:left;">
-                                                                        <div class="veticle-large-features-box">
-                                                                            <img src="../assets/images/features_icon_2.png" alt="features icon" />
+                                                                        <div class="trdmrk-box">
+                                                                            <img src="../assets/images/product/clnt-asso-management.jpg" alt="features icon" />
                                                                         </div>
                                                                         <div class="veticle-features-content">
-                                                                            <h4>Data Management<dd>Advanced data management for integrity and consistency</dd></h4>
+                                                                            <h4><strong style="color:#9bbb5c;">Data Management</strong><dd>Advanced data management for integrity and consistency</dd></h4>
                                                                         </div>
                                                                     </div>
                                                                 </div>
