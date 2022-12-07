77551f83052d90065c7346a0bd11b0121bee68f9
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-07 17:33:36 +0530

    include/header and menu inside footer section add link

--- about/about-02.php
+++ about/about-02.php
@@ -185,7 +185,7 @@
                                       <div class="why-us-box d-block position-relative
                                             px-md-4 px-3 pt-3">
                                           <img src="../assets/images/about/service-digital.png" alt="">
-                                          <h4 class="d-block pb-md-2">Full-Service Digital</h4>
+                                          <h4 class="d-block pb-md-2 pt-2">Full-Service Digital</h4>
                                             <span class="d-block">One house for the complete digital solution
                                                 which lowers cost and enables total ownership of
                                                 the project with superior quality and timely
@@ -196,7 +196,7 @@
                                         <div class="why-us-box d-block position-relative
                                                 px-md-4 px-3 pt-3">
                                                 <img src="../assets/images/about/erterprise-grade.png" alt="">
-                                                <h4 class="d-block pb-md-2">Enterprise-
+                                                <h4 class="d-block pb-md-2 pt-2">Enterprise-
                                                     Grade</h4>
                                                 <span class="d-block">Successful track record of
                                                     delivering mission-critical, scalable, digital
@@ -208,7 +208,7 @@
                                         <div class="why-us-box d-block position-relative
                                                 px-md-4 px-3 pt-3">
                                                  <img src="../assets/images/about/born-digital.png" alt="">
-                                                <h4 class="d-block pb-md-2">Born Digital</h4>
+                                                <h4 class="d-block pb-md-2 pt-2">Born Digital</h4>
                                                 <span class="d-block">Since 1997, we have been
                                                     offering new-age digital solutions to keep
                                                     your business digitally advanced and
