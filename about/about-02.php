04fa6cd12add0ee392b7c6881807555d4cab7238
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-07 15:21:02 +0530

    include/header and menu inside footer section add link

--- about/about-02.php
+++ about/about-02.php
@@ -250,9 +250,9 @@
                                                     <img  src="../assets/images/about/team-member.jpg" alt="">
                                               </div>
                                               <div class="content-details fadeIn-bottom">
-                                                  <img src="../assets/images/about/team-member.jpg" alt="">
-                                                  <h3 class="content-title">This is a title</h3>
-                                                  <p class="content-text">This is a short description</p>
+                                                    <h3 class="content-title">This is a title</h3>
+                                                    <p class="content-text">This is a short description</p>
+                                                    <img src="../assets/images/about/team-member.jpg" alt="">
                                               </div>
                                           </a>
                                       </div>
