2c0ba968917c1e907a7538de3e2cfb11ae37d412
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-12 16:22:00 +0530

    model

--- assets/js/customize.js
+++ assets/js/customize.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
-  $('ul.tabs li').click(function(){
+  $('ul.tabs3 li').click(function(){
     var tab_id= $(this).attr('data-tab');
-    $('ul.tabs li').removeClass('current');
+    $('ul.tabs3 li').removeClass('current');
     $('.tab-content3').removeClass('current');
     $(this).addClass('current');
     $("#"+tab_id).addClass('current');
