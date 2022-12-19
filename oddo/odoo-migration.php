6d2f6d01c1b2b54900191b09f3b6a725e4ba0a25
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-19 17:05:58 +0530

    odoo migration common.css and migration page banner

--- oddo/odoo-migration.php
+++ oddo/odoo-migration.php
@@ -39,11 +39,11 @@
                         <div class="col-lg-7 col-md-7">
                                 <h1 class="w-100 d-block pb-2" style="color:#ba1419;">Odoo Data Migration</h1>
                                 <ul class="d-block pl-0 py-md-4">
-                                    <li class="pb-2" style="color:#ba1419;">Data migration from legacy systems to Odoo ERP</li>
-                                    <li class="pb-2" style="color:#ba1419;">Data migration from other ERPs to Odoo</li>
-                                    <li class="pb-2" style="color:#ba1419;">Data migration from disconnected multiple apps to Odoo ERP</li>
-                                    <li class="pb-2" style="color:#ba1419;">Data migration from older version of Odoo to current stable version</li>
-                                    <li class="pb-2" style="color:#ba1419;">Data migration from Odoo Community to Odoo Enterprise</li>
+                                    <li class="pb-3" style="color:#ba1419;">Data migration from legacy systems to Odoo ERP</li>
+                                    <li class="pb-3" style="color:#ba1419;">Data migration from other ERPs to Odoo</li>
+                                    <li class="pb-3" style="color:#ba1419;">Data migration from disconnected multiple apps to Odoo ERP</li>
+                                    <li class="pb-3" style="color:#ba1419;">Data migration from older version of Odoo to current stable version</li>
+                                    <li class="pb-3" style="color:#ba1419;">Data migration from Odoo Community to Odoo Enterprise</li>
                                 </ul>
                         </div>
                     </div>
