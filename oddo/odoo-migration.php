0957516a07bd5fedb974803483d5bfff8c970488
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-20 16:06:09 +0530

    Migration page why choose us section

--- oddo/odoo-migration.php
+++ oddo/odoo-migration.php
@@ -63,12 +63,12 @@
                              <li class="pb-3">Is your current ERP system really paying off?</li>
                              <li class="pb-3">Is your current Odoo ERP version sufficient for your business growth?</li>
                        </ul>
-                       <p class="d-block">If your ERP system has become high maintenance,
+                       <p class="d-block mb-md-3 mb-2">If your ERP system has become high maintenance,
                            limits your flexibility, or inhibits customer service, then migration
                            is the only answer.</p>
-                       <p class="d-block mb-md-2 mb-1">It is true that migrating and cleaning up ERP data
+                       <p class="d-block mb-md-3 mb-2">It is true that migrating and cleaning up ERP data
                            can be costly, but it is worthwhile if performed meticulously.</p>
-                       <p class="d-block">If you are using Odoo or are planning to switch
+                       <p class="d-block mb-md-3 mb-2">If you are using Odoo or are planning to switch
                            to Odoo, ERP migration and upgradation are inevitable factors as Odoo
                            releases new versions yearly.</p>
                 </div>
