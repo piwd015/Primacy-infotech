fc1e7b77509c2c02909170cc16f594ce0a36750c
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-20 19:01:59 +0530

    Migration page why choose us section

--- oddo/odoo-integration.php
+++ oddo/odoo-integration.php
@@ -34,11 +34,12 @@
         <section class="integra-bg py-md-5 pt-5 pb-3 mt-md-0 mt-4">
                   <div class="container-fluid px-md-5 px-3 pt-md-5">
                        <div class="row d-flex flex-wrap flex-column-reverse flex-md-row">
-                               <div class="col-lg-6 col-md-6 d-flex flex-wrap align-items-center">
-                                      <h1 class="d-block pb-3">Odoo Integration Services</h1>
-                                      <h4 class="d-block pb-4">Helping you consolidate and coordinate your Business Operations</h4>
+                               <div class="col-lg-7 col-md-7 d-flex flex-wrap align-items-center">
+                                       <h1 class="d-block pb-3">Odoo Integration Services</h1>
+                                       <h4 class="d-block pb-4">Helping you consolidate and coordinate your Business Operations</h4>
+                                       <button class="button"><span>Let's Connect! </span></button>
                                </div>
-                               <div class="col-lg-6 col-md-6 pt-md-0 pt-5">
+                               <div class="col-lg-5 col-md-5 pt-md-0 pt-5">
                                        <div class="migra-bnr-Img">
                                               <img src="../assets/images/oddo/integra-banr-right-Img.png" alt="">
                                        </div>
