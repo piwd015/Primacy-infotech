e2a905ae75328b2ea9ffe7cb8af568a3af6d791d
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-21 17:52:04 +0530

    Migration page why choose us section

--- oddo/odoo-integration.php
+++ oddo/odoo-integration.php
@@ -218,29 +218,40 @@
 
                             <div class="tab-panels">
                                    <section id="payment" class="tab-panel">
-                                       <h2>6A. Märzen</h2>
-                                       <p><strong>Overall Impression:</strong> An elegant, malty German amber lager with a clean, rich, toasty and bready malt flavor, restrained bitterness, and a dry finish that encourages another drink. The overall malt impression is soft, elegant, and complex, with a rich aftertaste that is never cloying or heavy.</p>
-                                       <p><strong>History:</strong> As the name suggests, brewed as a stronger “March beer” in March and lagered in cold caves over the summer. Modern versions trace back to the lager developed by Spaten in 1841, contemporaneous to the development of Vienna lager. However, the Märzen name is much older than 1841; the early ones were dark brown, and in Austria the name implied a strength band (14 °P) rather than a style. The German amber lager version (in the Viennese style of the time) was first served at Oktoberfest in 1872, a tradition that lasted until 1990 when the golden Festbier was adopted as the standard festival beer.</p>
+                                       <div class="row paymnt-gatwy">
+                                             <h5 class="d-block pb-2">Payment Gateway Integration</h5>
+                                             <p class="d-block pb-4">Whether you need point of sale (POS) software
+                                                 integration or payment integration for an eCommerce site, Confianz Global
+                                                 is able to configure payment gateways that let customers pay you in their
+                                                 currency. This comes with a variety of security options so customers know
+                                                 they can make payments securely and reliably.
+                                             </p>
+                                             <div class="col-md-3">
+                                                   <div class="gatewy-box w-100 d-block px-3 py-md-4 py-3">
+                                                         <img src="/assets/images/oddo/pay-paypal.png" alt="">
+                                                          <h3 class="d-block py-3">PayPal</h3>
+                                                          <span class="d-block">An American multinational Financial
+                                                              technology company offering payment terminologies for
+                                                              operations used worldwide for both personal as well as
+                                                              business operations. Odoo Integration with PayPal helps
+                                                              your business to reach a newer customer base and ease in
+                                                              accepting as well as receiving payments.
+                                                          </span>
+                                                   </div>
+                                             </div>
+                                       </div>
                                    </section>
                                    <section id="social" class="tab-panel">
-                                       <h2>6B. Rauchbier</h2>
-                                       <p><strong>Overall Impression:</strong>  An elegant, malty German amber lager with a balanced, complementary beechwood smoke character. Toasty-rich malt in aroma and flavor, restrained bitterness, low to high smoke flavor, clean fermentation profile, and an attenuated finish are characteristic.</p>
-                                       <p><strong>History:</strong> A historical specialty of the city of Bamberg, in the Franconian region of Bavaria in Germany. Beechwood-smoked malt is used to make a Märzen-style amber lager. The smoke character of the malt varies by maltster; some breweries produce their own smoked malt (rauchmalz).</p>
+
                                    </section>
                                    <section id="sms" class="tab-panel">
-                                       <h2>6C. Dunkles Bock</h2>
-                                       <p><strong>Overall Impression:</strong> A dark, strong, malty German lager beer that emphasizes the malty-rich and somewhat toasty qualities of continental malts without being sweet in the finish.</p>
-                                       <p><strong>History:</strong> Originated in the Northern German city of Einbeck, which was a brewing center and popular exporter in the days of the Hanseatic League (14th to 17th century). Recreated in Munich starting in the 17th century. The name “bock” is based on a corruption of the name “Einbeck” in the Bavarian dialect, and was thus only used after the beer came to Munich. “Bock” also means “Ram” in German, and is often used in logos and advertisements.</p>
+
                                    </section>
                                    <section id="biometric" class="tab-panel">
-                                       <h2>6C. Dunkles Bock</h2>
-                                       <p><strong>Overall Impression:</strong> A dark, strong, malty German lager beer that emphasizes the malty-rich and somewhat toasty qualities of continental malts without being sweet in the finish.</p>
-                                       <p><strong>History:</strong> Originated in the Northern German city of Einbeck, which was a brewing center and popular exporter in the days of the Hanseatic League (14th to 17th century). Recreated in Munich starting in the 17th century. The name “bock” is based on a corruption of the name “Einbeck” in the Bavarian dialect, and was thus only used after the beer came to Munich. “Bock” also means “Ram” in German, and is often used in logos and advertisements.</p>
+
                                    </section>
                                     <section id="docparser" class="tab-panel">
-                                        <h2>6C. Dunkles Bock</h2>
-                                        <p><strong>Overall Impression:</strong> A dark, strong, malty German lager beer that emphasizes the malty-rich and somewhat toasty qualities of continental malts without being sweet in the finish.</p>
-                                        <p><strong>History:</strong> Originated in the Northern German city of Einbeck, which was a brewing center and popular exporter in the days of the Hanseatic League (14th to 17th century). Recreated in Munich starting in the 17th century. The name “bock” is based on a corruption of the name “Einbeck” in the Bavarian dialect, and was thus only used after the beer came to Munich. “Bock” also means “Ram” in German, and is often used in logos and advertisements.</p>
+
                                     </section>
                             </div>
                      </div>
