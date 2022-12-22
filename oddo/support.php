23595d9e25e3cf3fb505c7ac0d3d4532c762e45c
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-22 16:46:13 +0530

    Migration page why choose us section

--- oddo/support.php
+++ oddo/support.php
@@ -53,17 +53,46 @@
               <div class="container">
                      <h2 class="w-100 d-block text-center pb-4">Odoo ERP Support and Maintenance</h2>
                      <div class="row">
-                           <div class="col-md-6">
+                            <div class="col-md-6 mb-4">
                                  <ul class="srpt-mnts w-100 d-block">
-                                       <li class="pb-3">
+                                     <li class="pb-3">
                                            <img src="/assets/images/oddo/dedicated.png" alt="">
-                                           <strong class="w-100 d-block pb-2">Dedicated Support Team</strong>
+                                           <strong class="w-100 d-block pb-3">Dedicated Support Team</strong>
                                            You will have a dedicated support team to ensure
                                            all your issues are resolved on time without losing
                                            time waiting for support tickets.</li>
                                  </ul>
                            </div>
-                           <div class="col-md-6"></div>
+                            <div class="col-md-6 mb-4">
+                                   <ul class="srpt-mnts w-100 d-block">
+                                           <li class="pb-3">
+                                               <img src="/assets/images/oddo/monitoring.png" alt="">
+                                               <strong class="w-100 d-block pb-3">Ongoing Support and Monitoring</strong>
+                                               We ensure that we are always ready to address potential problems
+                                               quickly and efficiently throughout the implementation stages.
+                                           </li>
+                                   </ul>
+                           </div>
+                            <div class="col-md-6 mb-md-4 mb-4">
+                                  <ul class="srpt-mnts w-100 d-block">
+                                       <li class="pb-3">
+                                           <img src="/assets/images/oddo/system-checkups.png" alt="">
+                                           <strong class="w-100 d-block pb-3">System Checkups & Updates</strong>
+                                           We offer regular system checkups to ensure you get the most out of your
+                                           system and keep it updated as and when required.
+                                        </li>
+                                  </ul>
+                             </div>
+                            <div class="col-md-6">
+                                  <ul class="srpt-mnts w-100 d-block">
+                                         <li class="pb-3">
+                                             <img src="/assets/images/oddo/monitoring.png" alt="">
+                                             <strong class="w-100 d-block pb-3">Ongoing Support and Monitoring</strong>
+                                             We ensure that we are always ready to address potential problems
+                                             quickly and efficiently throughout the implementation stages.
+                                         </li>
+                                  </ul>
+                            </div>
                      </div>
               </div>
         </section>
