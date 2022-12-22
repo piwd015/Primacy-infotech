00e1f5571bf521acace7b62ca4744e0e5c3e6405
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-22 17:46:22 +0530

    Migration page why choose us section

--- oddo/support.php
+++ oddo/support.php
@@ -192,9 +192,9 @@
                     <div class="row d-flex flex-wrap flex-column-reverse flex-md-row">
                           <div class="col-md-3"></div>
                           <div class="col-md-9">
-                                <div class="supprt-box w-100 d-block py-md-5 py-3 px-md-4 px-3">
+                                <div class="supprt-box w-100 d-block p-md-5 p-3">
                                           <h2 class="d-block pb-2">Support from Odoo</h2>
-                                          <p class="d-block pb-3">Whenever you buy an online
+                                          <p class="d-block">Whenever you buy an online
                                               subscription to Odoo, you get free access to Odoo’s
                                               online support.
                                           </p>
