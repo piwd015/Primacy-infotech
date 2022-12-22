21c6179466e79ae9e296f114545371984e5d3d78
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-22 19:40:18 +0530

    Migration page why choose us section

--- oddo/support.php
+++ oddo/support.php
@@ -366,16 +366,19 @@
                                        <td>John</td>
                                        <td>Doe</td>
                                        <td>john@example.com</td>
+                                       <td>john@example.com</td>
                                    </tr>
                                    <tr>
                                        <td>Mary</td>
                                        <td>Moe</td>
                                        <td>mary@example.com</td>
+                                       <td>john@example.com</td>
                                    </tr>
                                    <tr>
                                        <td>July</td>
                                        <td>Dooley</td>
                                        <td>july@example.com</td>
+                                       <td>john@example.com</td>
                                    </tr>
                             </tbody>
                       </table>
