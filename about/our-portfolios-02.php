935473153ac6d428c1fdb5278ee5e7ef2ab9ccf6
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 17:28:05 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -44,7 +44,8 @@
                                  All
                           </label>
                           <div class="tab w-100 d-block">
-                                 <img src="/assets/images/about/portfolio-01.jpg" alt="">
+                              <h1>Tab Two Content</h1>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                           </div>
 
                           <input type="radio" name="tabs" id="tabtwo">
