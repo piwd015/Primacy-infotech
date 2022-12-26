ad4567096818ffa3742711b23acee766d9b3498a
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 13:01:21 +0530

    POPUP

---
+++ about/our-portfolios-02.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Primacy Infotech | Oddo Implementation In Kolkata | Affordable & Best ERP for SMEs | ERP  for Manufacturing Industry</title>
+    <meta name="description" content="Primacy Infotech provides Odoo ERP  for construction, manufacturing, Retail Industry, Schools, Hospitals. Our Odoo implementation includes evaluation, planning, configuration and data migration. ☆Ask - Odoo Demo +91-8088015866. " />
+    <meta name="keywords" content="erp development,erp software development,erp software development company,erp development company,odoo development services,erp development services,odoo erp development,erp implementation,erp software development company,erp implementation companies,custom erp software development,best erp software in india,erp software india,erp software development company in india,odoo development company in India,ERP management system
+" />
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="EN">
+    <meta name="organization" content="Primacy Infotech">
+    <link rel="canonical" href="../index.php">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Primacy Infotech Best website development company in kolkata">
+    <meta property="og:description" content="Primacy Infotech- It is a website design and development company based in kolkata. We help small and mid sized company to achieve thier marketing goals. Call today - 9088015866">
+    <meta property="og:image" content="assets/images/logo-p.png">
+    <meta property="og:url" content="index.html">
+    <meta property="og:site_name" content="Primacy Infotech">
+    <meta name="format-detection" content="telephone=no">
+    <style>
+        @media only screen and (max-width:767px) {
+            .oddo-imple-bnr {
+                margin-top: 50px;
+            }
+        }
+    </style>
+    <!-- ------start header section------- -->
+    <?php include '../include/header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../assets/css/common.css">
+    <script type="text/javascript" src="../assets/js/customize.js"></script>
+    <!-- ------end header section------- -->
+    <div class="page-body">
+           <section class="portflo-bg w-100 d-block text-center">
+                  <div class="container-fluid">
+                         <h1 class="w-100 d-block text-center mb-4">Our Portfolios</h1>
+                         <p class="d-block text-center">Our philosophy and expertise are best represented by our work</p>
+                  </div>
+           </section>
+    </div>
+    <!-- footer -->
+    <?php include '../include/menu.php'; ?>
+    <?php include '../include/footer.php'; ?>
+    <script>
+        $(".box").on({
+            mouseover:function(){
+                $(this).find("img:nth-child(1)").stop().animate({opacity:0},600);
+                $(this).find("img:nth-child(2)").stop().animate({opacity:1},600);
+            }, mouseout:function(){
+                $(this).find("img:nth-child(1)").stop().animate({opacity:1},600);
+                $(this).find("img:nth-child(2)").stop().animate({opacity:0},600);
+            }
+        });
+    </script>
+</head>
+</html>
