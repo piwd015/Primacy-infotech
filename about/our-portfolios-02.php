ffc5d9acef3b04f7f2aef167cc36c0ddeaddd330
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 19:21:04 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -163,7 +163,7 @@
                                                         </a>
                                                 </div>
                                         </div>
-                                        <div class="col-md-4 p-1 pb-2">
+                                        <div class="col-md-4 p-2 pb-2">
                                                 <div class="content">
                                                         <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
                                                                 <div class="content-overlay">
