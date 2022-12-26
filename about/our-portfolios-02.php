f2684e4d39bd8eafe7f3049ce7414ef219f7d7c7
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 16:01:00 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -50,14 +50,20 @@
                           </div>
 
                           <input type="radio" name="tabs" id="tabtwo">
-                          <label for="tabtwo">Web</label>
+                          <label for="tabtwo">
+                              <img src="/assets/images/about/logo.png" alt="">
+                              Web
+                          </label>
                           <div class="tab">
                               <h1>Tab Two Content</h1>
                               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                           </div>
 
                           <input type="radio" name="tabs" id="tabthree">
-                          <label for="tabthree">Mobile</label>
+                          <label for="tabthree">
+                              <img src="/assets/images/about/mobile.png" alt="">
+                              Mobile
+                          </label>
                           <div class="tab">
                               <h1>Tab Three Content</h1>
                               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
