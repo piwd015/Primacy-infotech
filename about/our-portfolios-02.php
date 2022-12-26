136d71d3eb13387c08cdd3069a122eb0fa2f0dde
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 13:47:16 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -39,7 +39,7 @@
                          <p class="d-block text-center">Our philosophy and expertise are best represented by our work</p>
                       <div class="tabs">
                           <input type="radio" name="tabs" id="tabone" checked="checked">
-                          <label for="tabone">Tab One</label>
+                          <label for="tabone">All</label>
                           <div class="tab">
                               <h1>Tab One Content</h1>
                               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -47,14 +47,21 @@
                           </div>
 
                           <input type="radio" name="tabs" id="tabtwo">
-                          <label for="tabtwo">Tab Two</label>
+                          <label for="tabtwo">Web</label>
                           <div class="tab">
                               <h1>Tab Two Content</h1>
                               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                           </div>
 
                           <input type="radio" name="tabs" id="tabthree">
-                          <label for="tabthree">Tab Three</label>
+                          <label for="tabthree">Mobile</label>
+                          <div class="tab">
+                              <h1>Tab Three Content</h1>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                          </div>
+
+                          <input type="radio" name="tabs" id="tabfour">
+                          <label for="tabfour">Mobile</label>
                           <div class="tab">
                               <h1>Tab Three Content</h1>
                               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
