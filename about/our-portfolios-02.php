afcc8eaf3a59585aca7799ae6d65dba17d0aa3ea
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 19:22:44 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -189,6 +189,45 @@
                                                         </a>
                                                 </div>
                                         </div>
+                                        <div class="col-md-4 p-2 pb-2">
+                                         <div class="content">
+                                             <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                                 <div class="content-overlay">
+                                                     <div class="content-details fadeIn-left">
+                                                         <h3 class="content-title">Global</h3>
+                                                         <p>Student Assistant</p>
+                                                     </div>
+                                                 </div>
+                                                 <img class="content-image" src="/assets/images/about/portfolio-01.jpg" alt="">
+                                             </a>
+                                         </div>
+                                     </div>
+                                        <div class="col-md-4 p-2 pb-2">
+                                         <div class="content">
+                                             <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                                 <div class="content-overlay">
+                                                     <div class="content-details fadeIn-left">
+                                                         <h3 class="content-title">Global</h3>
+                                                         <p>Student Assistant</p>
+                                                     </div>
+                                                 </div>
+                                                 <img class="content-image" src="/assets/images/about/portfolio-01.jpg" alt="">
+                                             </a>
+                                         </div>
+                                     </div>
+                                        <div class="col-md-4 p-2 pb-2">
+                                         <div class="content">
+                                             <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                                 <div class="content-overlay">
+                                                     <div class="content-details fadeIn-left">
+                                                         <h3 class="content-title">Global</h3>
+                                                         <p>Student Assistant</p>
+                                                     </div>
+                                                 </div>
+                                                 <img class="content-image" src="/assets/images/about/portfolio-01.jpg" alt="">
+                                             </a>
+                                         </div>
+                                     </div>
                                  </div>
                           </div>
 
