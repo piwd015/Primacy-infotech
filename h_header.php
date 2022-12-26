492fbdd4abfb63718c3201e83fbf477217e922a6
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 12:19:09 +0530

    POPUP

--- h_header.php
+++ h_header.php
@@ -82,7 +82,7 @@
                                                          <ul class="top-menu">
                                                                <li><a href="../about/about.php">About Us</a></li>
                                                                <li><a href="https://erp.primacyinfotech.com/jobs">Career</a></li>
-                                                               <li><a href="../about/our-portfolios">Portfolios</a></li>
+                                                               <li><a href="../about/our-portfolios">Our Portfolios</a></li>
                                                                <li><a href="../about/our-portfolios">Our Team</a></li>
                                                                <li><a href="https://erp.primacyinfotech.com/blog/">Blog</a></li>
                                                                <li><a href="../about/contact.php">Contact Us</a></li>
