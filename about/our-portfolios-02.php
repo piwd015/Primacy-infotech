0b7d642e1b7824bd401cecaccfbd55f4710cebe1
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-26 13:33:49 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -37,6 +37,30 @@
                   <div class="container-fluid">
                          <h1 class="w-100 d-block text-center pb-4 mb-4">Our Portfolios</h1>
                          <p class="d-block text-center">Our philosophy and expertise are best represented by our work</p>
+                      <div class="tabs">
+                          <input type="radio" name="tabs" id="tabone" checked="checked">
+                          <label for="tabone">Tab One</label>
+                          <div class="tab">
+                              <h1>Tab One Content</h1>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+
+                          <input type="radio" name="tabs" id="tabtwo">
+                          <label for="tabtwo">Tab Two</label>
+                          <div class="tab">
+                              <h1>Tab Two Content</h1>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                          </div>
+
+                          <input type="radio" name="tabs" id="tabthree">
+                          <label for="tabthree">Tab Three</label>
+                          <div class="tab">
+                              <h1>Tab Three Content</h1>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                          </div>
+                      </div>
+
                   </div>
            </section>
     </div>
