f4d77a3766abfae26f6c41f184ee3f9d8e2fcaca
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-28 14:10:46 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -242,9 +242,9 @@
                                                          </a>
                                                  </div>
                                          </div>
-                                        <div class="col-md-3 p-2 pb-2">
+                                         <div class="col-md-3 p-2 pb-2">
                                               <div class="content">
-                                                     <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                                     <a href="#" target="_blank">
                                                              <div class="content-overlay">
                                                                      <div class="content-details fadeIn-left">
                                                                             <dd>Mobile App</dd>
@@ -254,10 +254,10 @@
                                                             <img class="content-image" src="/assets/images/about/portfolio-app-01.jpg" alt="">
                                                      </a>
                                               </div>
-                                       </div>
+                                        </div>
                                         <div class="col-md-3 p-2 pb-2">
                                              <div class="content">
-                                                     <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
+                                                     <a href="#" target="_blank">
                                                              <div class="content-overlay">
                                                                      <div class="content-details fadeIn-left">
                                                                            <dd>Mobile App</dd>
@@ -268,20 +268,21 @@
                                                              <img class="content-image" src="/assets/images/about/portfolio-app-02.jpg" alt="">
                                                      </a>
                                              </div>
-                                     </div>
+                                        </div>
                                         <div class="col-md-3 p-2 pb-2">
-                                         <div class="content">
-                                             <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
-                                                 <div class="content-overlay">
-                                                     <div class="content-details fadeIn-left">
-                                                         <h3 class="content-title">Global</h3>
-                                                         <p>Student Assistant</p>
-                                                     </div>
-                                                 </div>
-                                                 <img class="content-image" src="/assets/images/about/portfolio-01.jpg" alt="">
-                                             </a>
-                                         </div>
-                                     </div>
+                                             <div class="content">
+                                                     <a href="#" target="_blank">
+                                                             <div class="content-overlay">
+                                                                     <div class="content-details fadeIn-left">
+                                                                           <dd>Mobile App</dd>
+                                                                           <h3 class="content-title">Delvfast</h3>
+                                                                            <p>Meat, Fish & more</p>
+                                                                     </div>
+                                                             </div>
+                                                             <img class="content-image" src="/assets/images/about/portfolio-app-03.jpg" alt="">
+                                                     </a>
+                                             </div>
+                                       </div>
                                  </div>
                           </div>
 
