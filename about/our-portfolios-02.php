29befbf183e85ddf6ee30f13f6007af4d289409d
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-28 12:56:14 +0530

    POPUP

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -235,18 +235,6 @@
                                                          <a href="#" target="_blank">
                                                                  <div class="content-overlay">
                                                                          <div class="content-details fadeIn-left">
-                                                                             <h3 class="content-title">Annodyne</h3>
-                                                                         </div>
-                                                                 </div>
-                                                                <img class="content-image" src="/assets/images/about/portfolio-logo-07.jpg" alt="">
-                                                         </a>
-                                                 </div>
-                                         </div>
-                                         <div class="col-md-3 p-2 pb-2">
-                                                 <div class="content">
-                                                         <a href="#" target="_blank">
-                                                                 <div class="content-overlay">
-                                                                         <div class="content-details fadeIn-left">
                                                                              <h3 class="content-title">Sbec</h3>
                                                                          </div>
                                                                  </div>
