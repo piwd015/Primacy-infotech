a7e3587f93ee206398ce2c67a6e3f082dba7e1b6
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-29 17:24:14 +0530

    about/portfolio-02.php

---
+++ include/odoo-common.php
@@ -0,0 +1,303 @@
+<section class="features w-100 d-block">
+       <div class="container why-chs py-md-4">
+        <h2 class="w-100 d-block text-center pb-4"><strong>ODOO Features</strong></h2>
+        <div class="row">
+            <div class="col-md-6 mb-md-5 mb-3">
+                <div class="odo-fturs" style="background:#4d7285;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#4d7285;"><img src="../assets/images/oddo/build-website-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Build stunning website</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left">Website</li>
+                        <li class="float-left">Blog</li>
+                        <li class="float-left">E-commerce</li>
+                        <li class="float-left">Forum</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 mb-md-5 mb-3">
+                <div class="odo-fturs" style="background:#8c3e74;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#8c3e74;"><img src="../assets/images/oddo/finance-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="w-100 d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Get control of your finances</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left w-100">Invoicing</li>
+                        <li class="float-left w-100">Accounting</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 mb-md-5 mb-3">
+                <div class="odo-fturs" style="background:#91450e;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#91450e;"><img src="../assets/images/oddo/sales-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Boost your sales</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left">CRM</li>
+                        <li class="float-left">Sales</li>
+                        <li class="float-left">Point of Sale</li>
+                        <li class="float-left">Subscriptions</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 mb-md-5 mb-3">
+                <div class="odo-fturs" style="background:#284068;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#284068;"><img src="../assets/images/oddo/human-resource-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="w-100 d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Human Resource</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left">Employees</li>
+                        <li class="float-left">Expences</li>
+                        <li class="float-left">Recruitment</li>
+                        <li class="float-left">Appraisals</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 mb-md-5 mb-3">
+                <div class="odo-fturs" style="background:#00b2a5;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#00b2a5;"><img src="../assets/images/oddo/operation-better-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="w-100 d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Run your operation better</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left">Project</li>
+                        <li class="float-left">Manufacturing</li>
+                        <li class="float-left">Warehouse</li>
+                        <li class="float-left">Purchase</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="odo-fturs" style="background:#b83820;">
+                    <div class="fturs-icon-bg">
+                        <dd style="background:#b83820;"><img src="../assets/images/oddo/marketing-icon.png" alt=""></dd>
+                    </div>
+                    <h2 class="w-100 d-block pt-md-4 pt-3 pl-md-0 pl-4 text-left">Boost Your Marketing</h2>
+                    <ul class="d-block pl-md-3 pl-4">
+                        <li class="float-left">Email Marketing</li>
+                        <li class="float-left">Expences</li>
+                        <li class="float-left">Recruitment</li>
+                        <li class="float-left">Appraisals</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="odo-srvs py-md-5 py-3">
+    <h2 class="w-100 d-block text-center">OUR ODOO ERP DEVELOPMENT</h2>
+    <h3 class="w-100 d-block text-center pb-3">SERVICES</h3>
+    <div class="container">
+        <div class="row pl-md-5">
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/customization-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Odoo Customization</h4>
+                        <p class="d-block text-center mb-0">We will fullfill your business Needs with our expertise!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="../oddo/oddo-implementation.php">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/implementation-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Odoo Implementation</h4>
+                        <p class="d-block text-center mb-0">We understand your businessRequirements! We customize the eachEach module to implement it in the wayyou want!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/integration-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Odoo Integration</h4>
+                        <p class="d-block text-center mb-0">We offer out of the box solution to Integrate Odoo with 3rd party application!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/migration-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Odoo Migration</h4>
+                        <p class="d-block text-center mb-0">We provide data and code Migrationfrom your legacy system!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/maintenance-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Odoo Support and Maintenance</h4>
+                        <p class="d-block text-center mb-0">We provide 24 X7 bug fixes and issue Resolving and version upgrade!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/consulting-service-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Consulting Services</h4>
+                        <p class="d-block text-center mb-0">Odoo provides sales management, CRM, HRMS, E-commerce, warehouse management, and accounting.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/consultation-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Licencing Consultation</h4>
+                        <p class="d-block text-center mb-0">We provide 24 X7 bug fixes and issue Resolving and version upgrade!</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-3 col-6 mb-3 px-2">
+                <a href="#implementationprocess">
+                    <div class="odo-srvs-box">
+                        <img src="../assets/images/oddo/hosting-icon.png" alt="">
+                        <h4 class="w-100 d-block text-center pt-md-4 pt-2 pb-md-2">Hosting</h4>
+                        <p class="d-block text-center mb-0">We provide 24 X7 bug fixes and issue Resolving and version upgrade!</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="odo-intgr w-100 d-block py-md-5 py-3">
+    <div class="container">
+        <h2 class="w-100 d-block text-center pb-md-4 pb-2"><strong>Industries</strong> we helped with <strong>Oddo</strong></h2>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/Manufacturing.png" alt=""><span class="w-100 d-block text-center pt-3">Manufacturing</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/retail.png" alt=""><span class="w-100 d-block text-center pt-3">Retail</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/wholesale.png" alt=""><span class="w-100 d-block text-center pt-3">Wholesale</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/E-Commerce.png" alt=""><span class="w-100 d-block text-center pt-3">E-Commerce</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/telecom.png" alt=""><span class="w-100 d-block text-center pt-3">Telecom</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/construction.png" alt=""><span class="w-100 d-block text-center pt-3">Construction</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/fmcg.png" alt=""><span class="w-100 d-block text-center pt-3">FMCG</span>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-6 mb-4">
+                <div class="indus-logo">
+                    <img src="../assets/images/oddo/trading.pmg" alt=""><span class="w-100 d-block text-center pt-3">Trading</span>
+                </div>
+            </div>
+            <!-- <div class="col-lg-3 col-md-3 col-6 mb-4">
+                   <div class="indus-logo">
+                            <img src="../assets/images/oddo/healthcare.png" alt=""><span class="w-100 d-block text-center pt-3">Healthcare</span>
+                   </div>
+            </div> -->
+        </div>
+    </div>
+</section>
+
+<section class="w-100 d-block pb-md-5 mb-md-5">
+    <div class="container odo-faq">
+        <h2 class="w-100 d-block text-center pb-md-4"><dd class="mr-md-3 mr-1"></dd>Frequently Asked Questions<dd class="ml-md-3 ml-1"></dd></h2>
+        <div id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="card">
+                <div class="card-header" role="tab" id="accordionHeadingOne">
+                    <div class="mb-0 row">
+                        <div class="col-12 no-padding accordion-head">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#accordionBodyOne" aria-expanded="false" aria-controls="accordionBodyOne"
+                               class="collapsed ">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                <h3><img src="../assets/images/oddo/consultance-icon.png" class="mr-3" alt="">Why should I choose Primacy Infotech for odoo implementation?</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="accordionBodyOne" class="collapse" role="tabpanel" aria-labelledby="accordionHeadingOne" aria-expanded="false" data-parent="accordion">
+                    <div class="card-block d-block col-12">
+                        <p>The availability of technical and functional consultants makes Primacy Infotech the best odoo implementation partner for your company. 8+ Years of experience and knowledge in every industry domains, make Primacy Infotech project undertakings fool-proof and robust. We always keep customer desires on top priority while making every decision.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" role="tab" id="accordionHeadingTwo">
+                    <div class="mb-0 row">
+                        <div class="col-12 no-padding accordion-head">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#accordionBodyTwo" aria-expanded="false" aria-controls="accordionBodyTwo"
+                               class="collapsed ">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                <h3><img src="../assets/images/oddo/freelancer-icon.png" class="mr-3" alt="">Why choose Odoo partners over freelancers for Odoo implementation?</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="accordionBodyTwo" class="collapse" role="tabpanel" aria-labelledby="accordionHeadingTwo" aria-expanded="false" data-parent="accordion">
+                    <div class="card-block col-12">
+                        <p>It is better to go for Odoo partners than freelancers for Odoo Implementation, as Odoo partners are more qualified and trained professionals. Being certified, Odoo Partners always adhere to standards and professional methodologies.Also, Odoo partners are experts in all industry domains and have access to Enterprise Edition source code, offering boundless Odoo services.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" role="tab" id="accordionHeadingThree">
+                    <div class="mb-0 row">
+                        <div class="col-12 no-padding accordion-head">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#accordionBodyThree" aria-expanded="false" aria-controls="accordionBodyThree"
+                               class="collapsed ">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                <h3><img src="../assets/images/oddo/friendly-icon.png" class="mr-3" alt="">What makes Odoo implementation pocket-friendly?</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="accordionBodyThree" class="collapse" role="tabpanel" aria-labelledby="accordionHeadingThree" aria-expanded="false" data-parent="accordion">
+                    <div class="card-block col-12">
+                        <p>You can lower the cost of Odoo implementation by adding only essential modules that are required for your business. Lesser the modules, lesser the Odoo implementation costs. With no license fees unlike proprietary ERPs, Odoo community edition further lowers down the cost of Odoo implementation. Odoo enterprise online edition, brings down further the expense, as no additional infrastructures are demanded.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" role="tab" id="accordionHeadingFour">
+                    <div class="mb-0 row">
+                        <div class="col-12 no-padding accordion-head">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#accordionBodyFour" aria-expanded="false" aria-controls="accordionBodyThree"
+                               class="collapsed ">
+                                <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                <h3><img src="../assets/images/oddo/erp-icon.png" class="mr-3" alt="">Why should I opt Odoo over other ERPs?</h3>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div id="accordionBodyFour" class="collapse" role="tabpanel" aria-labelledby="accordionHeadingFour" aria-expanded="false" data-parent="accordion">
+                    <div class="card-block col-12">
+                        <p>Odoo saves you with a lot of money, leaving them for utilizing in future customizations and other essentials. Secondly, Odoo offers the unlimited possibility of customizations in the ERP. Numerous custom feature modules help the business achieve business results with greater ease. Also with Odoo, there is no risk of getting locked in either by the vendor or technology.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
