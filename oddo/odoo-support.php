2a17185cded89d3ae3d7743d60d7d2a21553a048
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-29 19:26:20 +0530

    about/portfolio-02.php

--- oddo/odoo-support.php
+++ oddo/odoo-support.php
@@ -547,7 +547,7 @@
             }
         });
     </script>
-
+    <?php include '../include/odoo-common.php'; ?>
     <?php include '../include/menu.php'; ?>
     <?php include '../include/footer.php'; ?>
 </head>
