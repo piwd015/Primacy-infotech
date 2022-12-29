d371b5ce197cfa575a83080e5f90e7456563560f
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-29 12:39:30 +0530

    about/portfolio-02.php

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -533,11 +533,10 @@
                                                  <a href="https://unsplash.com/photos/HkTMcmlMOUQ" target="_blank">
                                                      <div class="content-overlay">
                                                          <div class="content-details fadeIn-left">
-                                                             <h3 class="content-title">Global</h3>
-                                                             <p>Student Assistant</p>
+                                                             <h3 class="content-title">Sns<br/> Ludo</h3>
                                                          </div>
                                                      </div>
-                                                     <img class="content-image" src="/assets/images/about/portfolio-01.jpg" alt="">
+                                                     <img class="content-image" src="/assets/images/about/portfolio-11.jpg" alt="">
                                                  </a>
                                              </div>
                                          </div>
