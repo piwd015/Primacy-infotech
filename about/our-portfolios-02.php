95295f4f0da91cafe16c96910b37477a5b403052
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-29 13:06:06 +0530

    about/portfolio-02.php

--- about/our-portfolios-02.php
+++ about/our-portfolios-02.php
@@ -35,7 +35,7 @@
     <div class="page-body">
            <section class="portflo-bg port-scr-srt w-100 d-block text-center">
                   <div class="container-fluid px-md-0 px-3">
-                         <h1 class="w-100 d-block text-center pb-4">Our Portfolios</h1>
+                         <h1 class="w-100 d-block text-center pb-md-4 pb-3">Our Portfolios</h1>
                          <p class="d-block text-center pt-4 pb-5">Our philosophy and expertise are best represented by our work</p>
                       <div class="tabs pt-4">
                           <input type="radio" name="tabs" id="tabone" checked="checked">
