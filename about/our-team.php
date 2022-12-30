207f9792e53af3e677507829b94b9e74f9c7deb7
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-30 13:13:28 +0530

    about/portfolio-02.php

--- about/our-team.php
+++ about/our-team.php
@@ -411,7 +411,7 @@
                 </div>
                 <ul class="row portfolio-item">
                     <li class="mix dev col-xl-3 col-md-4 col-12 col-sm-6 pd">
-                        <img src="../assets/images/about/office-tour/gallery-big-01.jpg" itemprop="thumbnail" alt="Image description" />
+                        <img src="../assets/images/about/office-tour/gallery-thumb-01.jpg" itemprop="thumbnail" alt="Image description" />
                         <div class="portfolio-overlay">
                             <div class="overlay-content">
                                 <p class="category">Project Name</p>
