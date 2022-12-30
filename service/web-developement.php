1217215570b5646e2033f39233fdf31641f891b3
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-30 17:21:54 +0530

    service/web-development.php

--- service/web-developement.php
+++ service/web-developement.php
@@ -300,7 +300,7 @@
 					    <div class="swiper-wrapper">
                                 <div class="swiper-slide">
                                 <div class="app-screenshot-item">
-                                    <a href="../assets/images/global.png" data-rel="lightbox-popup:screenshot"><img src="../assets/images/global.png" alt="web development"></a>
+                                    <a href="../assets/images/global.jpg" data-rel="lightbox-popup:screenshot"><img src="../assets/images/global.jpg" alt="web development"></a>
                                     <div class="asi-icon-wr"><span class="asi-icon"><i class="icon_plus"></i></span></div>
                                 </div>
                             </div>
