36f62560f8aa4b813776daf9b8d177242da2c5ee
Raju Primacy <piwd015@primacyinfotech.com>
2022-12-30 17:06:18 +0530

    assets/css/common.css

--- about/our-team.php
+++ about/our-team.php
@@ -555,6 +555,42 @@
                             </div>
                         </div>
                     </li>
+                      <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd">
+                           <img src="../assets/images/about/office-tour/gallery-thumb-05.jpg" itemprop="thumbnail" alt="Image description" />
+                           <div class="portfolio-overlay">
+                            <div class="overlay-content">
+                                <p class="category">Taki Park</p>
+                                <a href="https://primacyinfotech.com/" title="View Project" target="_blank">
+                                    <div class="magnify-icon">
+                                        <p><span><i class="fa fa-link" aria-hidden="true"></i></span></p>
+                                    </div>
+                                </a>
+                                <a data-fancybox="item" title="click to zoom-in" href="../assets/images/about/office-tour/gallery-big-05.jpg" data-size="1200x600">
+                                    <div class="magnify-icon">
+                                        <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </li>
+                      <li class="mix web col-xl-3 col-md-4 col-12 col-sm-6 pd">
+                           <img src="../assets/images/about/office-tour/gallery-thumb-10.jpg" itemprop="thumbnail" alt="Image description" />
+                           <div class="portfolio-overlay">
+                                <div class="overlay-content">
+                                        <p class="category">Taki Riverside</p>
+                                        <a href="https://primacyinfotech.com/" title="View Project" target="_blank">
+                                            <div class="magnify-icon">
+                                                <p><span><i class="fa fa-link" aria-hidden="true"></i></span></p>
+                                            </div>
+                                        </a>
+                                        <a data-fancybox="item" title="click to zoom-in" href="../assets/images/about/office-tour/gallery-big-10.jpg" data-size="1200x600">
+                                            <div class="magnify-icon">
+                                                <p><span><i class="fa fa-search" aria-hidden="true"></i></span></p>
+                                            </div>
+                                        </a>
+                                </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </section>
