150568b956f096f80a34d6c4da65b39779a733a8
Raju Primacy <piwd015@primacyinfotech.com>
2023-01-03 19:42:43 +0530

    about/portfolio.php

--- about/our-portfolios.php
+++ about/our-portfolios.php
@@ -587,6 +587,46 @@
                                     </a>
                                 </div>
                             </div>
+                            <div class="col-md-8 p-0 pb-2">
+                                <div class="content">
+                                    <a>
+                                        <div class="content-overlay">
+                                            <div class="content-details fadeIn-left">
+                                                <h3 class="content-title">Ajmu</h3>
+                                            </div>
+                                        </div>
+                                        <img class="content-image" src="/assets/images/about/portfolio-17.jpg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 p-0 pb-2">
+                                <div class="col-md-12">
+                                    <div class="content">
+                                        <a>
+                                            <div class="content-overlay">
+                                                <div class="content-details fadeIn-left">
+                                                    <h3 class="content-title">Samritmart</h3>
+                                                    <p>One stop online destination for all your grocery</p>
+                                                </div>
+                                            </div>
+                                            <img class="content-image" src="/assets/images/about/portfolio-03.jpg" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-12 pt-2">
+                                    <div class="content">
+                                        <a>
+                                            <div class="content-overlay">
+                                                <div class="content-details fadeIn-left">
+                                                    <h3 class="content-title">Happy</h3>
+                                                    <h3 class="content-title">Martz</h3>
+                                                </div>
+                                            </div>
+                                            <img class="content-image" src="/assets/images/about/portfolio-02.jpg" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
