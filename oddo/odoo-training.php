ce902d86de8d35a086c80eddb61691006321c532
Raju Primacy <piwd015@primacyinfotech.com>
2023-01-04 18:11:30 +0530

    assets/css/common.css

---
+++ oddo/odoo-training.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Primacy Infotech | Oddo Implementation In Kolkata | Affordable & Best ERP for SMEs | ERP  for Manufacturing Industry</title>
+    <meta name="description" content="Primacy Infotech provides Odoo ERP  for construction, manufacturing, Retail Industry, Schools, Hospitals. Our Odoo implementation includes evaluation, planning, configuration and data migration. ☆Ask - Odoo Demo +91-8088015866. " />
+    <meta name="keywords" content="erp development,erp software development,erp software development company,erp development company,odoo development services,erp development services,odoo erp development,erp implementation,erp software development company,erp implementation companies,custom erp software development,best erp software in india,erp software india,erp software development company in india,odoo development company in India,ERP management system
+" />
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="EN">
+    <meta name="organization" content="Primacy Infotech">
+    <link rel="canonical" href="../index.php">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Primacy Infotech Best website development company in kolkata">
+    <meta property="og:description" content="Primacy Infotech- It is a website design and development company based in kolkata. We help small and mid sized company to achieve thier marketing goals. Call today - 9088015866">
+    <meta property="og:image" content="assets/images/logo-p.png">
+    <meta property="og:url" content="index.html">
+    <meta property="og:site_name" content="Primacy Infotech">
+    <meta name="format-detection" content="telephone=no">
+    <!-- ------start header section------- -->
+    <?php include '../include/header.php'; ?>
+    <link rel="stylesheet" type="text/css" href="../assets/css/common.css">
+    <script type="text/javascript" src="../assets/js/customize.js"></script>
+    <style>
+        @media only screen and (max-width:767px) {
+            .oddo-imple-bnr {margin-top: 50px;}
+        }
+    </style>
+
+    <!--start model-->
+    <div id="myModal_grocery" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="text-danger" id="myModalLabel"><b>Submit Detail & Get discount</b></h3><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                </div>
+                <div class="modal-body">
+                      <form class="form-horizontal col-sm-12"  method="post">
+                        <div class="form-group"><label><b>Name</b></label>
+                             <input class="form-control required" placeholder="Your name" type="text" name="name" required></div>
+                             <div class="form-group"><label><b>E-Mail</b></label><input class="form-control email" placeholder="email@you.com (so that we can contact you)" type="email" name="email" required></div>
+                              <div class="form-group"><label><b>Phone</b></label><input class="form-control phone" placeholder="999-999-9999" name="phone" type="tel" required></div>
+                        <div class="form-group">
+                            <label><b>Select Services</b></label>
+                            <select name="services" class="form-control" required>
+                                <option value="">Select Services</option>
+                                <option value="Web Developemnt">Web Developemnt</option>
+                                <option value="Web Designing">Web Designing</option>
+                                <option value="MLM Softawre">MLM Softawre</option>
+                                <option value="Digital Marketing">Digital Marketing</option>
+                                <option value="Mobile APP Development">Mobile APP Development</option>
+                                <option value="Ecommerce Website">Ecommerce Website </option>
+                                <option selected value="Ecommerce Website">Odoo ERP </option>
+                                <option  value="Grocery Shop">Intellectual Property Management ERP </option>
+                                <option value="Grocery Shop">Grocery Shop</option>
+                                <option value="LMS">Learning Mamagement System</option>
+                                <option value="CMS">Content Management System</option>
+                                <option value="Food Delivery App">Food Delivery App</option>
+                                <option value="Loan Management Software">Loan Management Software</option>
+                                <option value="OLA/Uber Clone">OLA/Uber Clone</option>
+                                <option value="Travel Portal Solution">Travel Portal Solution</option>
+                                <option value="Recharge Portal">Recharge Portal</option>
+                                <option value="Real Estate Portal">Real Estate Portal</option>
+                                <option value="Video Portal">Video Portal</option>
+                                <option value="Billing Software">Billing Software</option>
+                                <option value="Accounting Portal">Accounting Portal</option>
+                            </select>
+                        </div>
+                        <div class="form-group"><label><b>Message</b></label>
+                              <textarea class="form-control" name="message" placeholder="Your message here.." required></textarea></div>
+                        <div class="form-group">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" name="quotes12" class="btn btn-success pull-right">Send It!</button>
+                            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end model-->
+
+    <!-- ------end header section------- -->
+    <div class="page-body mb-5">
+
+    </div>
+
+    <!-- footer -->
+
+    <!--start model end-->
+
+    <div id="myModal" class="modal fade modal_sec" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+
+                <div class="modal-header">
+
+                    <h3 id="myModalLabel"><b>Submit Detail & Get discount</b></h3><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+
+                </div>
+
+                <div class="modal-body">
+
+                    <form class="form-horizontal col-sm-12" method="post">
+
+                        <div class="form-group"><label><b>Name</b></label>
+
+                            <input class="form-control required" placeholder="Your name" type="text" name="name" required></div>
+
+                        <div class="form-group"><label><b>E-Mail</b></label><input class="form-control email" placeholder="email@you.com (so that we can contact you)"  type="email" name="email" required></div>
+
+                        <div class="form-group"><label><b>Phone</b></label><input class="form-control phone" placeholder="999-999-9999" name="phone" type="tel" required></div>
+
+                        <div class="form-group">
+
+                            <label>Select Services</label>
+
+                            <select name="services" class="form-control" required>
+
+                                <option value="" disabled="disabled">Select Services</option>
+
+                                <option value="Web Developemnt">Web Developemnt</option>
+
+                                <option value="Web Designing">Web Designing</option>
+
+                                <option value="Odoo ERP">Odoo ERP</option>
+
+                                <option value="Digital Marketing">Digital Marketing</option>
+
+                                <option value="Mobile APP Development">Mobile APP Development</option>
+                                <option value="Intellectual Property Management ERP">Intellectual Property Management ERP</option>
+                                <option value="Ecommerce Website">Ecommerce Website </option>
+
+                                <option value="Grocerry Shop">Grocerry Shop</option>
+
+                                <option value="LMS">Learning Mamagement System</option>
+
+                                <option value="CMS">Content Management System</option>
+
+                                <option value="Food Delivery App">Food Delivery App</option>
+
+                                <option value="Loan Management Software">Loan Management Software</option>
+
+                                <option value="OLA/Uber Clone">OLA/Uber Clone</option>
+
+                                <option value="Travel Portal Solution">Travel Portal Solution</option>
+
+                                <option value="Recharge Portal">Recharge Portal</option>
+
+                                <option value="Real Estate Portal">Real Estate Portal</option>
+
+                                <option value="Video Portal">Video Portal</option>
+
+                                <option value="Billing Software">Billing Software</option>
+
+                                <option value="Accounting Portal">Accounting Portal</option>\
+                                <option value="MLM Softawre">MLM Softawre</option>
+
+                            </select>
+
+                        </div>
+
+                        <div class="form-group"><label><b>Message</b></label>
+
+                            <textarea class="form-control" name="message" placeholder="Your message here.." required></textarea></div>
+
+                        <div class="form-group">
+
+                            <!--          <p class="help-block pull-left text-danger hide" id="form-error">&nbsp; The form is not valid. </p>-->
+
+                        </div>
+
+                        <div class="modal-footer">
+
+                            <input type="hidden" name="g-recaptcha-response" class="g-recaptcha-response">
+
+                            <button type="submit" name="submit11" class="btn btn-success pull-right">Send It!</button>
+
+                            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <!--My fixed header-->
+    <script>
+
+        $(document).ready(function(){
+            $("#myModal").find('form').find('select option[value="Odoo ERP"]').prop('selected', true);
+        });
+
+
+
+        $('.demo_slider').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+
+            ]
+        });
+
+        $('.pricing_slider').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+
+            ]
+        });
+
+
+        $('.features_slider').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+
+            ]
+        });
+
+
+
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
+            console.log(scroll >= 0);
+            if (scroll >= 950) {
+                $(".header_tab_sec").addClass("active");
+            } else {
+                $(".header_tab_sec").removeClass("active");
+            }
+        });
+    </script>
+    <?php include '../include/odoo-common.php'; ?>
+    <?php include '../include/menu.php'; ?>
+    <?php include '../include/footer.php'; ?>
+</head>
+</html>
+
