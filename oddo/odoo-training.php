c2129bbffa799de8ac902b960589cd84ac1b4369
Raju Primacy <piwd015@primacyinfotech.com>
2023-01-05 15:39:23 +0530

    assets/css/common.css

--- oddo/odoo-training.php
+++ oddo/odoo-training.php
@@ -146,8 +146,7 @@
                                         <li class="mb-4"><dd><span>4</span></dd>Live support Available</li>
                                    </ul>
                                   <div class="pln-bnft w-100 d-block">
-                                         <div class="col-md-4 trin-prs w-100 d-flex flex-wrap justify-content-center
-                                               align-items-center">
+                                         <div class="col-md-4 trin-prs w-100 pt-5">
                                                <h6 class="w-100 d-block text-center pb-3">Training Package</h6>
                                                <h2 class="w-100 d-block text-center pb-3"><sup>₹</sup>199</h2>
                                                <h4 class="w-100 d-block text-center pb-3">4 Hours</h4>
