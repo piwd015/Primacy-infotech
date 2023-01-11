07453c8bfabc5506ddaf3db67a667ad2e7287e71
Raju Primacy <piwd015@primacyinfotech.com>
2023-01-11 16:42:58 +0530

    assets/css/common.css

--- 404.php
+++ 404.php
@@ -22,7 +22,7 @@
     <meta property="twitter:image" content="../primacyinfotech.in/wp-content/uploads/2015/04/FotoJet.jpg">
     <!-- / Meta  -->
     <link rel="icon" href="../pilogofab.png" type="image/gif" sizes="HeightxWidth|any">
-    <?php include 'include/header.php'; ?>
+    <?php include 'h_header.php'; ?>
     <link rel="stylesheet" href="css/404.css">
 
     <body class="bg-purple w-100 d-block h-100">
