762c61b5d1eca78339f3503f6935b6618c255b1f
Raju Primacy <piwd015@primacyinfotech.com>
2023-01-11 16:57:10 +0530

    assets/css/common.css

--- 404.php
+++ 404.php
@@ -1,35 +1,32 @@
 <!DOCTYPE html>
 <html lang="en-US" class="mob">
 <hader>
-    <meta charset="utf-8" />
-    <!-- Primary Meta Tags -->
-    <title>Primacy Infotech Web, App & Odoo ERP Development Company</title>
-    <meta name="title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
-    <meta name="description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
-
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://primacyinfotech.com/">
-    <meta property="og:title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
-    <meta property="og:description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
-    <meta property="og:image" content="../primacyinfotech.in/wp-content/uploads/2015/04/FotoJet.jpg">
-
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://primacyinfotech.com/">
-    <meta property="twitter:title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
-    <meta property="twitter:description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
-    <meta property="twitter:image" content="../primacyinfotech.in/wp-content/uploads/2015/04/FotoJet.jpg">
-    <!-- / Meta  -->
-    <link rel="icon" href="../pilogofab.png" type="image/gif" sizes="HeightxWidth|any">
-
-    <link rel="stylesheet" href="css/404.css">
+<meta charset="utf-8" />
+<!-- Primary Meta Tags -->
+<title>Primacy Infotech Web, App & Odoo ERP Development Company</title>
+<meta name="title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
+<meta name="description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://primacyinfotech.com/">
+<meta property="og:title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
+<meta property="og:description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
+<meta property="og:image" content="../primacyinfotech.in/wp-content/uploads/2015/04/FotoJet.jpg">
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://primacyinfotech.com/">
+<meta property="twitter:title" content="Primacy Infotech Web, App & Odoo ERP Development Company">
+<meta property="twitter:description" content="Primacy Infotech Provide Enterprise-Grade High-quality, Stable and Scalable Web, App & Odoo ERP Development Services with Premium Support for our Clients. ">
+<meta property="twitter:image" content="../primacyinfotech.in/wp-content/uploads/2015/04/FotoJet.jpg">
+<!-- / Meta  -->
+<link rel="icon" href="../pilogofab.png" type="image/gif" sizes="HeightxWidth|any">
+<link rel="stylesheet" href="css/404.css">
 </hader>
     <body class="bg-purple w-100 d-block h-100">
     <div class="stars">
         <div class="central-body">
             <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px">
-            <a href="http://salehriaz.com/404Page/404.html" class="btn-go-home" target="_blank">GO BACK HOME</a>
+            <a href="https://primacyinfotech.com/" class="btn-go-home" target="_blank">GO BACK HOME</a>
         </div>
         <div class="objects">
             <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px">
@@ -47,10 +44,7 @@
             <div class="star"></div>
             <div class="star"></div>
             <div class="star"></div>
-
         </div>
-
     </div>
-
     </body>
 </html>
